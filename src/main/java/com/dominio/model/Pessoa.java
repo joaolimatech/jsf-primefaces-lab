503efbd840f8b4886401187b96166cc7b622bf8f
Joao Lima <joaolimatech@gmail.com>
2025-06-09 17:59:03 -0300

    First commit

---
+++ src/main/java/com/dominio/model/Pessoa.java
@@ -0,0 +1,9 @@
+package com.dominio.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable{ //oq é esse serializable?
+	
+	private static final long serialVersionUID = 1L;
+
+}
