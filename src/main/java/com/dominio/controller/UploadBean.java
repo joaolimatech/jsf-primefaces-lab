fcda01c955b408909503bfa18b411f0e12afcb58
Joao Lima <joaolimatech@gmail.com>
2025-06-25 07:53:43 -0300

    Classe propria para upload

---
+++ src/main/java/com/dominio/controller/UploadBean.java
@@ -0,0 +1,45 @@
+package com.dominio.controller;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import java.io.Serializable;
+
+
+
+//@ManagedBean(name="uploadBean") //pra buscar no xhtml com #{uploadBean.metodo}, sem esse cara@Managed o jsf nao sabe q essa classe existe
+@Named("uploadBean")
+@RequestScoped //O bean vive só durante a requisição HTTP.
+public class UploadBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L; 
+	
+	private UploadedFile file;
+	
+	public UploadedFile getFile()
+	{
+		return file;
+	}
+	
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+	
+//	public void handleFile() {
+//		if(file!=null) {
+//			System.out.println("Recebdio "+file.getFileName());
+//		} else {
+//			System.out.println("Nada recebido");
+//		}
+//	}
+	
+	public void handleFileUpload(FileUploadEvent event) {
+		UploadedFile file1 = event.getFile();
+		//application code
+		System.out.println("O arquivo se chama "+file1.getFileName());
+		this.file = file1;
+		}
+	
+}
